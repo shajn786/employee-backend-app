72e3ba6376ae7084311128477af2bb5c58b86476
shajn786 <shajn786@gmail.com>
2022-12-05 15:31:28 +0530

    initial

---
+++ src/main/java/com/example/employee_app_backend/EmployeeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/employee_app_backend/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.example.employee_app_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "welcome employee !!";
+    }
+
+
+
+
+}
